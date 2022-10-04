d25a9829b15288169047c9244f18a9fe95bc331d
AF Tamim <itamim345@gmail.com>
2022-10-04 06:20:30 +0530

    Taken all Selectors

---
+++ app.js
@@ -0,0 +1,13 @@
+//Selectors
+let incomeField = document.getElementById('income');
+let FoodField = document.getElementById('food');
+let rentField = document.getElementById('rent');
+let othersField = document.getElementById('others');
+calculateBtn = document.getElementById('calculate');
+let totalExpenseField = document.getElementById('total-expense');
+let avlAmountField = document.getElementById('avl-amount');
+let savingInputField = document.getElementById('savings-input');
+let saveBtn = document.getElementById('save-btn');
+let saveAmountBlock = document.getElementById('save-amount');
+let remainingAmountBlock = document.getElementById('remaining-amount');
+
