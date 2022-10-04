9e17ad8c4785a5374092967ee101b4bd4c1a6f77
AF Tamim <itamim345@gmail.com>
2022-10-04 06:32:53 +0530

    Total Expense Calculation done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Selectors
 let incomeField = document.getElementById('income');
-let FoodField = document.getElementById('food');
+let foodField = document.getElementById('food');
 let rentField = document.getElementById('rent');
 let othersField = document.getElementById('others');
-calculateBtn = document.getElementById('calculate');
+let calculateBtn = document.getElementById('calculate');
 let totalExpenseField = document.getElementById('total-expense');
 let avlAmountField = document.getElementById('avl-amount');
 let savingInputField = document.getElementById('savings-input');
@@ -11,3 +11,25 @@ let saveBtn = document.getElementById('save-btn');
 let saveAmountBlock = document.getElementById('save-amount');
 let remainingAmountBlock = document.getElementById('remaining-amount');
 
+//Event Handler
+calculateBtn.addEventListener('click', () => {
+    //taking user Income
+    let incomeText = incomeField.value;
+    let incomeAmount = parseFloat(incomeText);
+    // Taking Expenses
+    let foodText = foodField.value;
+    let foodAmount = parseFloat(foodText);
+
+    let rentText = rentField.value;
+    let rentAmount = parseFloat(rentText);
+
+    let ohtersText = othersField.value;
+    let othersAmount = parseFloat(ohtersText);
+
+    //Calculating Total Expense
+    let totalExpenseAmount = foodAmount + rentAmount + othersAmount;
+    
+    // Replacing Expense Amount to HTML
+    totalExpenseField.innerHTML = totalExpenseAmount;
+
+})
